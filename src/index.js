a82fec8b8363386ddf6911742e0ba45643f7d743
yehor-germanenko <foreman270478@gmail.com>
2021-12-30 12:46:47 +0200

    Send pdf as attachment

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import "regenerator-runtime/runtime.js";
 import React from 'react';
 
 import AWS from "aws-sdk";
-import { nanoid } from "nanoid";
+import nodemailer from "nodemailer";
 import ReactPDF from '@react-pdf/renderer';
 
 import { pdfComponents } from "./pdfs";
 
-const s3 = new AWS.S3();
-const ses = new AWS.SES();
+const transporter = nodemailer.createTransport({
+	SES: new AWS.SES()
+});
 
 const getBaseComponent = (components, component) => {
 	if (component in components) {
@@ -19,6 +20,17 @@ const getBaseComponent = (components, component) => {
 		message: `No such template defined with name ${component}`
 	};
 };
+
+const sendMail = (mailOptions) => {
+	return new Promise((resolve, reject)=>{
+		transporter.sendMail(mailOptions, (err, info) => {
+			if (err) {
+				reject(err);
+			}
+			resolve(info);
+		});
+	});
+}
 
 const renderPdf = async (component, data) => {
 	const rootElemComponent = React.createElement(component, data);
@@ -53,58 +65,25 @@ export const handler = async (event, context, callback) => {
 				statusCode: err.statusCode,
 				body: err.message
 			});
-			return;
 		}
 
 		const buffer = await renderPdf(reactTemplate, {dat: {...data.data.dat, title: data.data.title }});
 
-		const s3Params = {
-			Bucket: "pdfs.christee",
-			Key: `${nanoid()}.pdf`,
-			Body: buffer,
-			ContentType: "application/pdf",
-			ServerSideEncryption: "AES256"
-		};
-
-		const s3Res = await s3.upload(s3Params, (err) => {
-			if (err) {
-				return callback(null, {
-					statusCode: err.statusCode,
-					body: err.message
-				});
-			}
-		}).promise();
-
-		const sesParams = {
-			Source: 'no-reply@askchristee.com',
-			Destination: {
-				ToAddresses: [data.email]
+		const mailOptions = {
+			from: "no-reply@askchristee.com",
+			to: data.email,
+			subject: "SES Example",
+			text: "Your pdf is here",
+			attachments: {
+				filename: `${data.data.dat.meta.engine}-report.pdf`,
+				content: buffer,
+				contentType: "application/pdf",
 			},
-			ReplyToAddresses: [],
-			Message: {
-				Body: {
-					Html: {
-						Charset: "UTF-8",
-						Data: `Pdf report is <a href="${s3Res.Location}">here</a>.`
-					}
-				},
-				Subject: {
-					Charset: 'UTF-8',
-					Data: 'SES Example'
-				}
-			}
 		};
 
-		const sesRes = await ses.sendEmail(sesParams, err => {
-			if (err) {
-				return callback(null, {
-					statusCode: err.statusCode,
-					body: err.message
-				});
-			}
-		});
+		const mailRes = await sendMail(mailOptions);
 
-		if (sesRes) {
+		if (mailRes) {
 			callback(null, {
 				statusCode: 200,
 				body: "Link to pdf has been sent to your email!"
@@ -114,3 +93,61 @@ export const handler = async (event, context, callback) => {
 		return context.fail(err);
 	}
 };
+
+// Pdf as link
+
+/*
+const s3Params = {
+    Bucket: "pdfs.christee",
+    Key: `${nanoid()}.pdf`,
+    Body: buffer,
+    ContentType: "application/pdf",
+    ServerSideEncryption: "AES256"
+};
+
+
+const s3Res = await s3.upload(s3Params, (err) => {
+    if (err) {
+        return callback(null, {
+            statusCode: err.statusCode,
+            body: err.message
+        });
+    }
+}).promise();
+
+const sesParams = {
+    Source: 'no-reply@askchristee.com',
+    Destination: {
+        ToAddresses: [data.email]
+    },
+    ReplyToAddresses: [],
+    Message: {
+        Body: {
+            Html: {
+                Charset: "UTF-8",
+                Data: `Pdf report is <a href="${s3Res.Location}">here</a>.`
+            }
+        },
+        Subject: {
+            Charset: 'UTF-8',
+            Data: 'SES Example'
+        }
+    }
+};
+
+const sesRes = await ses.sendEmail(sesParams, err => {
+    if (err) {
+        return callback(null, {
+            statusCode: err.statusCode,
+            body: err.message
+        });
+    }
+});
+
+if (sesRes) {
+    callback(null, {
+        statusCode: 200,
+        body: "Link to pdf has been sent to your email!"
+    })
+}
+*/
